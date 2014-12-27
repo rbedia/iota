966f8b4144d6d871b99092b3250bce2678d5803b
Rafael Bedia <dcat@trillinux.org>
2014-12-27 17:00:18 -0500

    Removed unused undo() method.

--- src/main/java/org/doxu/iota/board/Board.java
+++ src/main/java/org/doxu/iota/board/Board.java
@@ -119,13 +119,6 @@ public class Board {
         return calculateScore(moves);
     }
 
-    public void undo(Laydown laydown) {
-        List<Move> moves = laydown.getMoves();
-        for (Move move : moves) {
-            board.applyCard(move.getLocation(), Card.BLANK);
-        }
-    }
-
     private int calculateScore(List<Move> moves) {
         int sum = 0;
         int lots = 0;
