6ada427ab0a8aaae3cca0cb828a7356f4ae9e5a0
Rafael Bedia <dcat@trillinux.org>
2014-12-30 23:15:25 -0500

    Made Rack more generic by allowing it to draw any list of cards.

--- src/main/java/org/doxu/iota/ui/Rack.java
+++ src/main/java/org/doxu/iota/ui/Rack.java
@@ -5,25 +5,29 @@ import java.awt.Graphics;
 import java.util.List;
 import javax.swing.JPanel;
 import org.doxu.iota.Card;
+import org.doxu.iota.Hand;
 import org.doxu.iota.Player;
 
 /**
- * Display for a player's available cards.
+ * Display for a list of cards.
  */
 public class Rack extends JPanel {
 
-    private final Player player;
+    private final List<Card> cards;
 
-    public Rack(Player player) {
-        this.player = player;
-        Dimension dim = new Dimension((CardRenderer.CARD_WIDTH + CardRenderer.INSET) * 4, CardRenderer.CARD_WIDTH + 5);
+    public Rack(List<Card> cards) {
+        this.cards = cards;
+        int count = Math.max(cards.size(), Hand.MAX_HAND);
+        Dimension dim = new Dimension((CardRenderer.CARD_WIDTH + CardRenderer.INSET) * count, CardRenderer.CARD_WIDTH + 5);
         setPreferredSize(dim);
         setMaximumSize(dim);
     }
 
-    private void draw(Graphics g) {
-        List<Card> cards = player.getHand().getCards();
+    public Rack(Player player) {
+        this(player.getHand().getCards());
+    }
 
+    private void draw(Graphics g) {
         for (int i = 0; i < cards.size(); i++) {
             Graphics g2card = g.create(i * CardRenderer.CARD_WIDTH, 0, CardRenderer.CARD_WIDTH, CardRenderer.CARD_WIDTH);
             CardRenderer.draw(g2card, cards.get(i));
