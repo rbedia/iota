d17899ccbb1f34455f8593d8ce713b1bd848ef62
Rafael Bedia <dcat@trillinux.org>
2015-01-18 20:09:25 -0500

    Implemented equals() and hashCode() so that it is consistent with compareTo().

--- iota-game/src/main/java/org/doxu/iota/player/ScoreLaydown.java
+++ iota-game/src/main/java/org/doxu/iota/player/ScoreLaydown.java
@@ -18,4 +18,23 @@ public class ScoreLaydown implements Comparable<ScoreLaydown> {
         return o.score == score ? 0 : o.score < score ? -1 : 1;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.score;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ScoreLaydown other = (ScoreLaydown) obj;
+        return this.score == other.score;
+    }
+
 }
