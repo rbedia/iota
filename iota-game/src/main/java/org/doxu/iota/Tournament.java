ea14a79024758d144d524266bb5087a8d6f993d0
Rafael Bedia <dcat@trillinux.org>
2015-01-31 18:55:21 -0500

    Implemented player order randomization.

--- iota-game/src/main/java/org/doxu/iota/Tournament.java
+++ iota-game/src/main/java/org/doxu/iota/Tournament.java
@@ -1,40 +1,55 @@
 package org.doxu.iota;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.doxu.iota.player.MinimizerPlayer;
+import org.doxu.iota.player.RandomPlayer;
 import org.doxu.iota.player.SimpleHighFourPlayer;
 import org.doxu.iota.player.SimpleHighThreePlayer;
-import org.doxu.iota.player.SimpleHighTwoPlayer;
 
 public class Tournament {
 
     public void run(List<Player> players, int games) {
-        int[] wins = new int[players.size()];
+        Map<Player, PlayerWins> wins = new HashMap<>();
+        for (Player player : players) {
+            wins.put(player, new PlayerWins(player));
+        }
 
         for (int i = 0; i < games; i++) {
             Game game = new Game();
-            // TODO switch the order of players
+            Collections.shuffle(players);
             game.init(players);
             game.deal();
             game.playStartingCard();
             while (!game.step()) {
-
             }
             Player winner = game.getWinner();
-            int index = players.indexOf(winner);
-            wins[index]++;
+            PlayerWins playerWins = wins.get(winner);
+            playerWins.wins++;
         }
 
-        for (int i = 0; i < wins.length; i++) {
-            Player player = players.get(i);
-            int win = wins[i];
-            System.out.println("Player " + player.getDisplayName() + " wins: " + win);
+        for (PlayerWins playerWins : wins.values()) {
+            Player player = playerWins.player;
+            int win = playerWins.wins;
+            System.out.println("Player " + player.getName() + " wins: " + win);
         }
     }
 
-//    private static class Results {
-//        Map<Player, Integer> wins = new HashMap<>();
-//    }
+    private static class PlayerWins {
+
+        int wins;
+
+        Player player;
+
+        public PlayerWins(Player player) {
+            this.player = player;
+            this.wins = 0;
+        }
+    }
+
     public static void main(String[] args) {
         Tournament tournament = new Tournament();
 
@@ -42,7 +57,8 @@ public class Tournament {
 
         players.add(new SimpleHighFourPlayer());
         players.add(new SimpleHighThreePlayer());
-        players.add(new SimpleHighTwoPlayer());
+        players.add(new MinimizerPlayer());
+        players.add(new RandomPlayer());
 
         int games = 1000;
 
